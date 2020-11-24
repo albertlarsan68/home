f2b55fa02de36fe31852647651c965ccd1a4ac60
albertlarsan68 <74931857+albertlarsan68@users.noreply.github.com>
2020-11-24 16:41:58 +0100

    Created a separate config file for the list of items

---
+++ config/items.php
@@ -0,0 +1,21 @@
+<?php
+return(
+    array(
+        array(
+            'location'  => 'albertlarsan',
+            'text'      => 'AlbertLarsan.test'
+        ),
+        array(
+            'location'  => 'dashboard',
+            'text'      => 'Page d\'accueil XAMPP'
+        ),
+        array(
+            'location'  => 'files',
+            'text'      => 'Gestion des fichiers'
+        ),
+        array(
+            'location'  => 'phpmyadmin',
+            'text'      => 'phpMyAdmin'
+        )
+    )
+);
--- index.php
+++ index.php
@@ -13,24 +13,7 @@ require __DIR__ . '/src/itemList.php';
 try {
     if (isset($_GET['action'])) {
         if ($_GET['action']=='listItems') {
-            $items=array(
-                array(
-                    'location'  => 'albertlarsan',
-                    'text'      => 'AlbertLarsan.test'
-                ),
-                array(
-                    'location'  => 'dashboard',
-                    'text'      => 'Page d\'accueil XAMPP'
-                ),
-                array(
-                    'location'  => 'files',
-                    'text'      => 'Gestion des fichiers'
-                ),
-                array(
-                    'location'  => 'phpmyadmin',
-                    'text'      => 'phpMyAdmin'
-                )
-            );
+            $items=require('config/items.php');
             $list=new ItemList($items);
             echo $list->ItemList();
         } elseif ($_GET['action']=='goto') {
