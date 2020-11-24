dbaa22352b732ddf2b6db0417b7d1d4888addfc2
albertlarsan68 <74931857+albertlarsan68@users.noreply.github.com>
2020-11-24 20:34:22 +0100

    Create items.php

---
+++ config.default/items.php
@@ -0,0 +1,12 @@
+<?php
+return(array(
+  array(
+    'location'=> '<your folder>',
+    'text' => '<the text that is shown>'
+  ),
+  array(
+    'location'=> '<your second folder>',
+    'text' => '<the second text that is shown>'
+  )
+  //...
+));
