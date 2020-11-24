980a78a9457214c2e9c4db4ea5ea416d2c7a0b84
albertlarsan68 <74931857+albertlarsan68@users.noreply.github.com>
2020-11-24 10:07:50 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$version=1;
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+try {
+    if (isset($_GET['action'])) {
+        if ($_GET['action']=='listItems') {
+            require($uri.'/home/itemList.php');
+            ItemList(array(
+                '1' => array('location' => 'albertlarsan', 'text' => 'AlbertLarsan.test'),
+            ));
+        } elseif ($_GET['action']=='goto') {
+            if (isset($_GET['location'])) {
+                header('Location: '.$uri.'/'.$_GET['location']);
+            }
+        }
+        
+    } else {
+        header("Location: ?action=listItems");
+        
+    }
+    
+} catch (\Throwable $th) {
+    echo "Erreur : ".$th->getMessage();
+}
